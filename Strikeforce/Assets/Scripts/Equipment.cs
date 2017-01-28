4fa29b1ee88c50d30b8eec30e8996cf34f28d63e
Nic Bunting <steelmagnum@hotmail.com>
2017-01-27 16:21:22 -0800

    Added EquipmentSlot class and modified Equipment class to work with EquipmentSlot

--- Strikeforce/Assets/Scripts/Equipment.cs
+++ Strikeforce/Assets/Scripts/Equipment.cs
@@ -5,6 +5,11 @@ namespace Strikeforce
 {
     public class Equipment : MonoBehaviour
     {
-
+		public Size SlotSize { get; set; }
+        public bool IsRemovable { get; protected set; }
+		
+		public Equipment(int width, int height) {
+			this.SlotSize = new Size(width, height);
+		}
     }
 }
---
+++ Strikeforce/Assets/Scripts/EquipmentSlot.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+namespace Strikeforce
+{
+    public class EquipmentSlot : MonoBehaviour
+    {
+        public Vector2 Location { get; protected set; }
+        public int Width { get { return EquippedItems.GetLength(1); } }
+        public int Height { get { return EquippedItems.GetLength(0); } }
+		public Equipment[,] EquippedItems { get; protected set; }
+		
+		public EquipmentSlot(int x, int y, int width, int height) {
+			this.Location = new Vector2(x, y);
+			this.EquippedItems = new Equipment[width, height];
+		}
+
+		public bool IsEquippable(Equipment item, int row, int column) {
+			Size slotSize = item.SlotSize;
+			int itemWidth = slotSize.Width;
+            if (itemWidth > this.Width)
+            {
+                return false;
+            }
+
+			int itemHeight = slotSize.Height;
+            if (itemHeight > this.Height)
+            {
+                return false;
+            }
+
+            for (int x = column; x < itemWidth; x++)
+            {
+                for (int y = row; y < itemHeight; y++)
+                {
+                    if (EquppedItems[x, y] != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+		}
+
+        public void Equip(Equipment item, int row, int column)
+        {
+            Size slotSize = item.SlotSize;
+			int itemWidth = slotSize.Width;
+            int itemHeight = slotSize.Height;
+
+            for(int x = column; x < itemWidth; x++) {
+                for (int y = row; y < itemHeight; y++)
+                {
+                    EquippedItems[x, y] = item;
+                }
+            }
+        }
+
+        public Equipment Unequip(int row, int column)
+        {
+            Equipment item = EquippedItems[row, column];
+            bool isRemovable = item.IsRemovable;
+            if (isRemovable == false)
+            {
+                return;
+            }
+
+            for (int x = 0; x < this.Width; x++)
+            {
+                for (int y = 0; y < this.Height; y++)
+                {
+                    Equipment itemToCheck = EquippedItems[x, y];
+                    if (itemToCheck.equals(item) == false)
+                    {
+                        continue;
+                    }
+
+                    EquippedItems[x, y] = null;
+                }
+            }
+
+            return item;
+        }
+    }
+}
