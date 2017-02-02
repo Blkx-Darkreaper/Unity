cf770cb9ea2deec9dd6949b36e23dc9da420061a
Nicolaj Bunting <steelmagnum@hotmail.com>
2017-02-01 18:04:14 -0800

    Added AssetManager class to load game assets upon load

---
+++ Strikeforce/Assets/Scripts/AssetManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Strikeforce
+{
+    public class AssetManager : MonoBehaviour
+    {
+        public Texture2D healthy, damaged, critical;
+
+        public void Awake()
+        {
+            // Load all game assets
+            LoadHealthBarTextures();
+        }
+
+        protected void LoadHealthBarTextures()
+        {
+            GlobalAssets.HealthBarTextures.Healthy = healthy;
+            GlobalAssets.HealthBarTextures.Damaged = damaged;
+            GlobalAssets.HealthBarTextures.Critical = critical;
+        }
+    }
+}
