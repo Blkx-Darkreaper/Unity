9dae909b1db4610f3d9bdabf16ed3ca34a6e2047
Nic Bunting <steelmagnum@hotmail.com>
2017-02-20 14:52:59 -0800

    Updated project file

--- Strikeforce/Assets/Scripts/Sector.cs
+++ Strikeforce/Assets/Scripts/Sector.cs
@@ -8,12 +8,14 @@ namespace Strikeforce
         public Vector2 Location { get; protected set; }
         public Size Size {get; protected set;}
         public Spawnpoint Spawn { get; protected set; }
+        public Player Owner { get; protected set; }
 
         public Sector(int x, int y, int width, int height, Spawnpoint spawn)
         {
             this.Location = new Vector2(x, y);
             this.Size = new Size(width, height);
             this.Spawn = spawn;
+            this.Owner = null;
         }
     }
 }
